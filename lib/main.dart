3b78e06ec0168f2678ab3387508e04cf6db515a6
yeon <a18311831a@gmail.com>
2024-11-27 17:51:45 +0900

    Feat.kakao

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,19 @@ import 'package:smu_flutter/screen/root_screen.dart';
 import 'package:smu_flutter/screen/login_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:smu_flutter/firebase_options.dart';
+import 'package:smu_flutter/screen/logo_screen.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+  // 웹 환경에서 카카오 로그인을 정상적으로 완료하려면 runApp() 호출 전 아래 메서드 호출 필요
+  WidgetsFlutterBinding.ensureInitialized();
+  // runApp() 호출 전 Flutter SDK 초기화
+  KakaoSdk.init(
+    nativeAppKey: '28352c19e374b93489a539774edbd01f'
+  );
   runApp(MyApp());
 }
 
@@ -30,8 +39,12 @@ class MyApp extends StatelessWidget {
   }
 
   final GoRouter _router = GoRouter(
-    initialLocation: '/login',
+    initialLocation: '/logo',
     routes: [
+      GoRoute(
+        path: '/logo',
+        builder: (context, state) => LogoScreen(),
+      ),
       GoRoute(
         path: '/login',
         builder: (context, state) => LoginScreen(),
